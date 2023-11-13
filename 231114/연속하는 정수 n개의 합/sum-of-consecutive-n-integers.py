ab33036af8fb322b974077c67e43de4d2f5391c1
YSW <101538076+YSW2@users.noreply.github.com>
2023-11-14 02:46:11 +0900

    Title: 연속하는 정수 n개의 합, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 231114/연속하는 정수 n개의 합/sum-of-consecutive-n-integers.py
+++ 231114/연속하는 정수 n개의 합/sum-of-consecutive-n-integers.py
@@ -6,7 +6,7 @@ arr_sum = arr[0]
 answer = 0
 
 for i in range(n):
-    while j < n and arr_sum != m:
+    while j < n and arr_sum < m:
         arr_sum += arr[j]
         j += 1
     if arr_sum == m:

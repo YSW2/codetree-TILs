156991995f58d3c19cc4b755cab8dc210e7d34fb
YSW <101538076+YSW2@users.noreply.github.com>
2023-11-14 02:45:40 +0900

    Title: 연속하는 정수 n개의 합, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231114/연속하는 정수 n개의 합/sum-of-consecutive-n-integers.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+j = 1
+arr_sum = arr[0]
+answer = 0
+
+for i in range(n):
+    while j < n and arr_sum != m:
+        arr_sum += arr[j]
+        j += 1
+    if arr_sum == m:
+        answer += 1
+    arr_sum -= arr[i]
+
+print(answer)

bf696a152e6294b60aa8b073c08ce062f8866196
YSW <101538076+YSW2@users.noreply.github.com>
2023-11-14 02:20:11 +0900

    Title: 부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231114/부분수열 여부 판단하기/determine-subsequence.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr_n = list(map(int, input().split()))
+arr_m = list(map(int, input().split()))
+
+j = 0
+for i in range(n):
+    if arr_n[i] == arr_m[j]:
+        j += 1
+
+if j == m:
+    print("Yes")
+
+else:
+    print("No")

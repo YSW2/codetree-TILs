ac8283019bef873e006e153ff66e98892bb5084e
YSW <101538076+YSW2@users.noreply.github.com>
2023-11-15 01:17:28 +0900

    Title: 0에 가장 가까운 합, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 231115/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -5,6 +5,8 @@ answer = arr[n-1]
 
 j = n-1
 for i in range(n):
+    if i == j:
+        break
     answer = min(abs(arr[i]+arr[j]), answer)
     while j > i and arr[i] + arr[j] > 0:
         j -= 1

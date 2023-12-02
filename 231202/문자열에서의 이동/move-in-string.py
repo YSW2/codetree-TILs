5b02dbe023268aab2ea01442871927899d77ef03
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-02 19:04:48 +0900

    Title: 문자열에서의 이동, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 231202/문자열에서의 이동/move-in-string.py
+++ 231202/문자열에서의 이동/move-in-string.py
@@ -3,21 +3,26 @@ arr = list(input())
 dp = [0 for x in range(n)]
 
 def find_near(arr, i):
-    string = ['a', 'b', 'c']
+    string = ['c', 'b', 'a']
     target = (string.index(arr[i])+1)%3
-    while i < len(arr):
-        if arr[i] == string[target]:
-            return i
-        i += 1
+    min_val = float("inf")
+    count = 0
 
-    return -1
+    while i >= 0:
+        if arr[i] == string[target] and dp[i] != -1:
+            min_val = min((dp[i] + count ** 2), min_val)
+        i -= 1
+        count += 1
+    
+    if min_val == float("inf"):
+        return -1
 
-
-for i in range(n-2, -1, -1):
-    idx = find_near(arr, i)
-    if idx == -1:
-        dp[i] = -1
     else:
-        dp[i] = dp[idx] + (idx-i) ** 2
+        return min_val
 
-print(dp[0])
+
+for i in range(1, n):
+    val = find_near(arr, i)
+    dp[i] = val
+
+print(dp[n-1])

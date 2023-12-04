9e16986213657f986cd831a1cec6f9e9d07677e5
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-04 18:51:07 +0900

    Title: 지그재그로 이동하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/지그재그로 이동하기/moving-with-zigzag.py
+++ 231204/지그재그로 이동하기/moving-with-zigzag.py
@@ -5,7 +5,7 @@ go = 1
 answer = 0
 
 while True:
-    if abs(go) >= abs(b):
+    if b < 0 and go <= b or b >= 0 and b <= go:
         answer += abs(loc - b)
         break
     answer += abs(go - loc)

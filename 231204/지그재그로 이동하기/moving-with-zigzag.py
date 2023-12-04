f92df68e3ba410cb1cbb6a104d01a0e80fca4691
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-04 18:47:52 +0900

    Title: 지그재그로 이동하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231204/지그재그로 이동하기/moving-with-zigzag.py
+++ 231204/지그재그로 이동하기/moving-with-zigzag.py
@@ -1,15 +1,14 @@
 a, b = map(int, input().split())
-loc = a
+b -= a
+loc = 0
 go = 1
 answer = 0
 
 while True:
-    move = abs(a + go - loc)
-    loc = a + go
-    answer += move
-    if abs(loc-a) >= abs(b-a):
-        answer -= abs(loc - b)
+    if abs(go) >= abs(b):
+        answer += abs(loc - b)
         break
+    answer += abs(go - loc)
+    loc = go
     go *= -2
-
 print(answer)

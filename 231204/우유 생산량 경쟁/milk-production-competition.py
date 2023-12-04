e257bbb2a470a823c39f9e234883f6facb4b8cad
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-04 19:21:41 +0900

    Title: 우유 생산량 경쟁, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/우유 생산량 경쟁/milk-production-competition.py
+++ 231204/우유 생산량 경쟁/milk-production-competition.py
@@ -5,14 +5,13 @@ board = ["Bessie", "Elsie", "Mildred"]
 answer = 0
 
 for _ in range(n):
-    record.append(list(input().split()))
+    day, name, score = input().split()
+    record.append([int(day), name, int(score)])
 
-record.sort(key=lambda x:x[0])
-
+record.sort()
+    
 for day, name, score in record:
-    score = int(score[1]) * (1 if score[0] == '+' else -1)
     cow[name] += score
-
     board_temp = [n for n, s in list(cow.items()) if s >= max(cow.values())]
     if board != board_temp:
         answer += 1

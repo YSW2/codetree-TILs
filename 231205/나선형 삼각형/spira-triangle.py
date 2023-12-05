fcb7304ef92aaffa1fa7032465c9c017ec558dd0
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-05 16:55:57 +0900

    Title: 나선형 삼각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231205/나선형 삼각형/spira-triangle.py
+++ 231205/나선형 삼각형/spira-triangle.py
@@ -1,21 +1,21 @@
 n = int(input())
-dp = [0 for x in range(n)]
+dp = [0 for x in range(n+1)]
 
 def recursive(n):
     if dp[n] != 0:
         return dp[n]
     
     else:
-        if n <= 2:
+        if n <= 3:
             dp[n] = 1
             return 1
 
-        elif n <= 4:
+        elif n <= 5:
             dp[n] = 2
             return 2
 
         else:
             dp[n] = recursive(n-1) + recursive(n-5)
 
-recursive(n-1)
-print(dp[n-1])
+recursive(n)
+print(dp[n])

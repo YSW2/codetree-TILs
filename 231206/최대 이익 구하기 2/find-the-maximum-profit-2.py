8f872ff7aeaa4658889b7847d1b4956a1e089ac9
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-06 15:21:02 +0900

    Title: 최대 이익 구하기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231206/최대 이익 구하기 2/find-the-maximum-profit-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = []
+answer = 0
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+def greedy(n, arr, day, pay):
+    global answer
+
+    if day >= n:
+        if day == n:
+            answer = max(answer, pay)
+        return
+    
+    for i, (t, p) in enumerate(arr):
+        greedy(n, arr[i+t:], day+i+t, pay+p)
+
+greedy(n, arr, 0, 0)
+print(answer)

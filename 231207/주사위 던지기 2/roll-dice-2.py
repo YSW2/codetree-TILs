2e6142f4093ef844f82ba735e722ae5b3ad7cbcb
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-07 18:34:22 +0900

    Title: 주사위 던지기 2, Time: 5058ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 231207/주사위 던지기 2/roll-dice-2.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+
+def throw(arr, n, m):
+    if sum(arr) > m:
+        return
+    
+    if len(arr) == n and sum(arr) == m:
+        for num in arr:
+            print(num, end=" ")
+        print()
+        return
+    
+    for i in range(1, 7):
+        arr.append(i)
+        throw(arr, n, m)
+        arr.pop()
+
+throw([], n, m)

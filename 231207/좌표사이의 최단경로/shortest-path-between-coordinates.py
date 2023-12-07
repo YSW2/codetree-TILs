929f133171025437fa1f67aa69988a66d6a91a44
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-07 18:14:13 +0900

    Title: 좌표사이의 최단경로, Time: 5075ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 231207/좌표사이의 최단경로/shortest-path-between-coordinates.py
+++ 231207/좌표사이의 최단경로/shortest-path-between-coordinates.py
@@ -2,21 +2,24 @@ a, b = map(int, input().split())
 go = [(1, 0), (0, 1)]
 answer = 0
 
+s = min(a, b)
+f = max(a, b)
+
 def search(i, j):
     global answer
 
     if i > j:
         return
 
-    if i > b or j > b:
+    if i > f or j > f:
         return
 
-    if (i, j) == (b, b):
+    if (i, j) == (f, f):
         answer += 1
     
     for go_y, go_x in go:
         search(i+go_y, j+go_x)
 
-search(a, a)
+search(s, s)
 
 print(answer)

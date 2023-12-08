f48cd84ed4b3c1428fc0d929b8492ddac4d34021
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-09 03:20:54 +0900

    Title: Breakout!, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231209/Breakout!/breakout!.py
+++ 231209/Breakout!/breakout!.py
@@ -2,38 +2,38 @@ import copy
 
 n = int(input())
 arr = list(map(int, input().split()))
+
+idx = 0
+
+while idx < len(arr):
+    if arr[idx] > -1:
+        temp = arr[idx]
+        temp_idx = idx
+        
+        while temp_idx >= 0 and temp >= 0:
+            arr[temp_idx] = temp
+            temp -= 1
+            temp_idx -= 1
+
+            if arr[temp_idx] >= 0:
+                break
+        
+    idx += 1
+
 arr2 = copy.copy(arr)
-no_answer = False
 
-if arr[0] > 0:
-    print(-1)
+for i in range(len(arr)):
+    if arr[i] == -1:
+        arr[i] = 0
 
-else:
-    idx = 0
-    val = 0
-    while idx < len(arr):
-        if no_answer:
-            print(-1)
-            break
-        if arr[idx] > -1:
-            temp = arr[idx]
-            temp_idx = idx
-            while temp >= 0 and temp_idx >= 0:
-                if arr[temp_idx] != temp:
-                    no_answer = True
-                    break
-                arr[temp_idx] = temp
-                arr2[temp_idx] = temp
-                temp_idx -= 1
-                temp -= 1
-            val = arr[idx]
-            idx += 1
-                
-        else:
-            arr2[idx] = 0
-            arr[idx] = val
-            val += 1
-            idx += 1
+val = 0
 
-    if not no_answer:
-        print(arr.count(0), arr2.count(0))
+for i in range(len(arr2)):
+    if arr2[i] == -1:
+        arr2[i] = val
+        val += 1
+    else:
+        val = 0
+
+#print(arr, arr2)
+print(arr2.count(0), arr.count(0))

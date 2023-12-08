a8f3c6babb5c9edabf2b02c647ea72f693971493
YSW <101538076+YSW2@users.noreply.github.com>
2023-12-09 03:04:32 +0900

    Title: Breakout!, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231209/Breakout!/breakout!.py
+++ 231209/Breakout!/breakout!.py
@@ -3,6 +3,7 @@ import copy
 n = int(input())
 arr = list(map(int, input().split()))
 arr2 = copy.copy(arr)
+no_answer = False
 
 if arr[0] > 0:
     print(-1)
@@ -11,10 +12,16 @@ else:
     idx = 0
     val = 0
     while idx < len(arr):
+        if no_answer:
+            print(-1)
+            break
         if arr[idx] > -1:
             temp = arr[idx]
             temp_idx = idx
             while temp >= 0 and temp_idx >= 0:
+                if arr[temp_idx] != temp:
+                    no_answer = True
+                    break
                 arr[temp_idx] = temp
                 arr2[temp_idx] = temp
                 temp_idx -= 1
@@ -28,4 +35,5 @@ else:
             val += 1
             idx += 1
 
-    print(arr.count(0), arr2.count(0))
+    if not no_answer:
+        print(arr.count(0), arr2.count(0))

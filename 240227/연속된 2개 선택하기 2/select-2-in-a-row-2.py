608852c0bfb1e4974503ba3cdebf3a5373135f88
YSW <101538076+YSW2@users.noreply.github.com>
2024-02-27 19:57:46 +0900

    Title: 연속된 2개 선택하기 2, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240227/연속된 2개 선택하기 2/select-2-in-a-row-2.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(float(input()))
+
+answer = arr[0]
+temp = 1
+max_index = 0
+
+for i in range(n):
+    temp *= arr[i]
+    if answer < temp:
+        answer = temp
+        max_index = i
+
+temp = answer
+for j in range(max_index):
+    temp /= arr[j]
+    if answer < temp:
+        answer = temp
+
+print(round(answer, 4))

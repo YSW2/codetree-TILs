a7defb9ddfd89c25050073dea62d270a870a6eaf
YSW <101538076+YSW2@users.noreply.github.com>
2024-03-09 14:25:18 +0900

    Title: 주사위와 재귀함수, Time: 661ms, Memory: 31MB, Status: Passed - CodeTree

--- 240309/주사위와 재귀함수/dice-and-recursive-function.py
+++ 240309/주사위와 재귀함수/dice-and-recursive-function.py
@@ -4,15 +4,17 @@ def roll(x, y, result):
             for n in result:
                 print(n, end=" ")
             print()
-            return
-    
-    if sum(result) > y:
         return
-        
+
     for i in range(1, 7):
         result.append(i)
-        roll(x-1, y, result)
-        result.pop()
+        total = sum(result)
+        if total > y:
+            result.pop()
+            return
+        else:
+            roll(x-1, y, result)
+            result.pop()
 
 x, y = map(int, input().split())
 
